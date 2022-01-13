f426634dc66f21c4bfc7b1d4015b7aaf1e9f9aa7
BRIAN SWARTZ <bdswartz72@gmail.com>
2022-01-13 14:23:47 -0500

    code refactor to cut size of js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,93 +1,25 @@
-
-// Function to validate the character class(es) input by the user
-var validateUserInput = function(userInput) {
-    if (userInput === "y" || userInput === "n") {
-        return true;
-    }
-    else {
-        return false;
-    };
-};
+var userChoice = [
+    ["special" , false],
+    ["upper case" , false],
+    ["lower case" , false],
+    ["numeric" , false]
+];
 
 // Determine the password length by prompting user for input
     var passwordLengthInput = function() {
-        var input = window.prompt("How long would you like your password to be (minimum 8 characters and maximum of 128 characters).")
-        input = parseInt(input);
+        var input = parseInt(prompt("How long would you like your password to be (minimum 8 characters and maximum of 128 characters)."));
         if (isNaN(input) || input < 8 || input > 128) {
-            window.alert("Please enter a number between 8 and 128 for password length.")
-            return passwordLengthInput();
+            alert("Please enter a number between 8 and 128 for password length.");
+            passwordLengthInput();
         }
-        else {
-            return input;
-        };
+        return input;
     };
 
 // Determine the password character class(es) criteria (Capital, Lowercase, Numeric, 
 // and special characters) by prompting user for input
-    var isSpecialCharInput = function() {
-        var input = window.prompt("Would you like to include special characters in your password? (type 'y' for yes or 'n' for no)");
-        input = input.toLowerCase();
-        var specialCharOutput = validateUserInput(input)
-        if (specialCharOutput) {
-            if (input === 'y') {
-                return true;
-            }
-            else {
-                return false;
-            };
-        }
-        else {
-            window.alert("Please enter y for yes or n for no.");
-            return isSpecialCharInput();
-        }
-    };
-
-    var isUpperCaseInput = function() {
-        var input = window.prompt("Would you like to include UPPERCASE letters in your password? (type 'y' for yes or 'n' for no)")
-        input = input.toLowerCase();
-        if (validateUserInput(input)) {
-            if (input === 'y') {
-                return true;
-            }
-            else {
-                return false;
-            };
-        }
-        else {
-            window.alert("Please enter y for yes or n for no.");
-            return isUpperCaseInput();
-        };
-    };
-    var isLowerCaseInput = function() {
-        input = window.prompt("Would you like to include lowercase letters in your password? (type 'y' for yes or 'n' for no)")
-        input = input.toLowerCase();
-        if (validateUserInput(input)) {
-            if (input === 'y') {
-                return true;
-            }
-            else {
-                return false;
-            };
-        }
-        else {
-            window.alert("Please enter y for yes or n for no.");
-            return isLowerCaseInput();
-        };
-    };
-    var isNumericInput = function() {
-        input = window.prompt("Would you like to include numeric characters (0-9) in you password? (type 'y' for yes or 'n' for no)")
-        input = input.toLowerCase();
-        if (validateUserInput(input)) {
-            if (input === 'y') {
-                return true;
-            }
-            else {
-                return false;
-            };
-        }
-        else {
-            window.alert("Please enter y for yes or n for no.");
-            return isNumericInput();
+    var userCharChoice = function() {
+        for (var index = 0; index < userChoice.length; index++) {
+            userChoice [index] [1] = confirm("Confirm that you would like to include " + userChoice [index] [0] +  " characters in your password?");
         };
     };
 
@@ -99,43 +31,31 @@ var generateDecCode = function() {
 
 // Build the password string by comparing Decimal Codes to the user selected 
 // character class(es) and building the password string
-var generatePassword = function (passwordLength, specialCharFlag, upperCaseFlag, 
-    lowerCaseFlag, numericFlag) {
+var generatePassword = function (passwordLength) {
     // Compare letter code to criteria
     var stringLength = 0;
     var passwordString = "";
-    // console.log(passwordLength)
-    // console.log(typeof(specialCharFlag));
-    // console.log(typeof(upperCaseFlag));
-    // console.log(typeof(lowerCaseFlag));
-    // console.log(typeof(numericFlag));
     while (stringLength < passwordLength) {
         // Generate a letter code for comparison to criteria
         var decCode = generateDecCode();
         // Generate a letter from Decimal Code and add it to our password string
-        // console.log(decCode);
-        if (decCode >= 48 && decCode <= 57 && numericFlag) {
+        if (decCode >= 48 && decCode <= 57 && userChoice [3] [1]) {
             passwordString = passwordString + String.fromCharCode(decCode);
             var stringLength = stringLength + 1;
-            // console.log(passwordString);
         }
-        else if (decCode >= 65 && decCode <= 90 && upperCaseFlag) {
+        else if (decCode >= 65 && decCode <= 90 && userChoice [1] [1]) {
             passwordString = passwordString + String.fromCharCode(decCode);
             var stringLength = stringLength + 1;
-            // console.log(passwordString);
         }
-        else if (decCode >= 97 && decCode <= 122 && lowerCaseFlag) {
+        else if (decCode >= 97 && decCode <= 122 && userChoice [2] [1]) {
             passwordString = passwordString + String.fromCharCode(decCode);
             var stringLength = stringLength + 1;
-            // console.log(passwordString);
         }
-        else if ((decCode >= 33 && decCode <= 47 || decCode >= 58 && decCode <= 64 || decCode >= 91 && decCode <= 96 || decCode >= 123 && decCode <= 126) && specialCharFlag) {
+        else if ((decCode >= 33 && decCode <= 47 || decCode >= 58 && decCode <= 64 || decCode >= 91 && decCode <= 96 || decCode >= 123 && decCode <= 126) && userChoice [0] [1]) {
             passwordString = passwordString + String.fromCharCode(decCode);
             var stringLength = stringLength + 1;
-            // console.log(passwordString);
         }
         else {
-            // console.log("continue")
         };
     };
      // pass a full password string back to the writePassword function
@@ -144,30 +64,22 @@ var generatePassword = function (passwordLength, specialCharFlag, upperCaseFlag,
 
 
 // Write the password string to the #password input
-function writePassword() {
+function writePassword() {  
     var passwordLength = passwordLengthInput();
-    // console.log(passwordLength);
-    var specialCharFlag = isSpecialCharInput();
-    // console.log(specialCharFlag);
-    var upperCaseFlag = isUpperCaseInput();
-    // console.log(upperCaseFlag);
-    var lowerCaseFlag = isLowerCaseInput();
-    // console.log(lowerCaseFlag);
-    var numericFlag = isNumericInput();
-    // console.log(numericFlag);
-    if (specialCharFlag || upperCaseFlag || lowerCaseFlag || numericFlag) {
-    var password = generatePassword(passwordLength, specialCharFlag, upperCaseFlag, 
-        lowerCaseFlag, numericFlag);
-    // console.log("password is " + password.length);
-    var passwordText = document.querySelector("#password");
-    passwordText.value = password;
+    // Populate the array userChoice [n] [1] with the user input flags
+    userCharChoice ();
+    // Check to make sure user chose at least one tranche of characters
+    if (userChoice [0] [1] || userChoice [1] [1] || userChoice [2] [1] || userChoice [3] [1]) {
+        var password = generatePassword(passwordLength);
+        console.log(password.length);
+        var passwordText = document.querySelector("#password");
+        passwordText.value = password;
     }
     else {
         window.alert("You must choose at least one character class in order to generate a password.");
         writePassword();
     }
 };
-
 // Initiate the program on click of button by the user
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
